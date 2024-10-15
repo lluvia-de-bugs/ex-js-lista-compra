20767e36952efc618f59907f27473ddd3c81b3f1
Sergi <sergi.prof@hotmail.com>
2024-10-15 16:10:51 +0200

    ends Guard

--- script.js
+++ script.js
@@ -6,32 +6,52 @@ function printList() {
     shopListDOM.innerHTML += `<li>${item} <span class="item-delete-btn">x</span> </li>`;
   }
 }
-
 // Función para eliminar un item de la lista
 function deleteItemFromList(item) {}
-
+function capitalizerFirst(text) {
+  const firstLetter = text.charAt(0);
+  const rest = text.slice(1);
+  return firstLetter.toUpperCase() + rest.toLowerCase();
+}
 // Función para agregar un item a la lista
 function addItemToList() {
   const inputDOM = document.getElementById("inputId");
-  const newItem = inputDOM.value.trim
+  const newItem = inputDOM.value.trim();
   inputDOM.value = "";
-
-  console.log(newItem.trim())
-
+  console.log(newItem.trim());
   //Guard
-  if (!newItem){
-    alert("debes introducir algo")
+  if (!newItem) {
+    alert("debes introducir algo");
     return;
   }
-  items.push(newItem)
+  if (newItem.length >= 25) {
+    alert("Mucho Texto")
+    return;
+  }
+  items.push(capitalizerFirst(newItem));
   printList();
   console.log(items);
 }
-
 // Función principal - Aquí empieza la aplicación
 function main() {
   printList();
 }
-
 // Llamada a la función principal
 main();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
